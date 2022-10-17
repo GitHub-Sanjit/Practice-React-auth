a4f9b1f57dba6d944b3d63542a1e7bc90d82082a
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-17 17:02:23 +0600

    59-5 Implement Login Sign Up and Sign Out

--- ema-john-with-router/src/components/Login/Login.js
+++ ema-john-with-router/src/components/Login/Login.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/UserContext";
 import "./Login.css";
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    // console.log(email, password);
+
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        // navigate("/");
+        form.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="form-container">
       <h2 className="form-title">Log In</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input
@@ -18,7 +40,7 @@ const Login = () => {
           />
         </div>
         <div className="form-control">
-          <label htmlFor="email">Password</label>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             name="password"
--- ema-john-with-router/src/components/SignUp/SignUp.js
+++ ema-john-with-router/src/components/SignUp/SignUp.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/UserContext";
 import "./SignUp.css";
 
 const SignUp = () => {
   const [error, setError] = useState(null);
+  const { createUser } = useContext(AuthContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -22,6 +24,16 @@ const SignUp = () => {
       setError("Your Password did not Match");
       return;
     }
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
