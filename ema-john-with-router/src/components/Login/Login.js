f699d632bbb92d8f563e22aa46e94ed346d4f09f
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-17 14:46:33 +0600

    Setup Auth Context under UserContext to share auth info

--- ema-john-with-router/src/components/Login/Login.js
+++ ema-john-with-router/src/components/Login/Login.js
@@ -5,7 +5,7 @@ import "./Login.css";
 const Login = () => {
   return (
     <div className="form-container">
-      <h2 className="form-title">Login</h2>
+      <h2 className="form-title">Log In</h2>
       <form>
         <div className="form-control">
           <label htmlFor="email">Email</label>
@@ -27,7 +27,7 @@ const Login = () => {
             required
           />
         </div>
-        <input className="btn-submit" type="submit" value="Login" />
+        <input className="btn-submit" type="submit" value="Log In" />
       </form>
       <p>
         New to ema john <Link to="/signup">Create a New Account</Link>
--- ema-john-with-router/src/components/SignUp/SignUp.js
+++ ema-john-with-router/src/components/SignUp/SignUp.js
@@ -1,8 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
-  return <div></div>;
+  return (
+    <div className="form-container">
+      <h2 className="form-title">SignUp</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm Password</label>
+          <input
+            type="password"
+            name="confirm"
+            id="password"
+            placeholder="password"
+            required
+          />
+        </div>
+        <input className="btn-submit" type="submit" value="Sign Up" />
+      </form>
+      <p>
+        Already Have an Account? <Link to="/login">Login</Link>
+      </p>
+    </div>
+  );
 };
 
 export default SignUp;
