c7486298d1e0083e76b843bd0551c696b0e172ac
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-17 13:01:25 +0600

    59-1 Module Introduction firebase setup and git clone

---
+++ ema-john-with-router/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDN8DFCUP7RjgxZnnsEU4drqe42NK1H8q8",
+  authDomain: "ema-john-simple-4e4ef.firebaseapp.com",
+  projectId: "ema-john-simple-4e4ef",
+  storageBucket: "ema-john-simple-4e4ef.appspot.com",
+  messagingSenderId: "896401080249",
+  appId: "1:896401080249:web:cbfeb4f5d441faf27a0068",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
