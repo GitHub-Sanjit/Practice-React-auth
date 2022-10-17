86fee3ae8f9f766939f366c849ae58f16cad943d
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-17 13:38:57 +0600

    59-2 create Login component and style Login form

--- ema-john-with-router/src/components/Login/Login.js
+++ ema-john-with-router/src/components/Login/Login.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import './Login.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Login</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email"
+            required
+          />
         </div>
-    );
+        <div className="form-control">
+          <label htmlFor="email">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            required
+          />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
+      </form>
+      <p>
+        New to ema john <Link to="/signup">Create a New Account</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
