ebf205f5b5a7320911b189fa30b050d89aa952a8
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-18 22:37:06 +0600

    60-3 Module Overview, Project setup and router setup

---
+++ dragon-news-client/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0I3DHl-Mjan6rKLEe9mXlrCgTQxFrbc0",
+  authDomain: "dragon-news-ec375.firebaseapp.com",
+  projectId: "dragon-news-ec375",
+  storageBucket: "dragon-news-ec375.appspot.com",
+  messagingSenderId: "1052581728418",
+  appId: "1:1052581728418:web:9ade644249a09be73a47fb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
