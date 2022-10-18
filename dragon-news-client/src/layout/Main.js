26ec2b5cda59788ded0d6d91b4e290e7b11c848c
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-18 23:08:39 +0600

    60-4 Create Layout Folder Structure and Basic Components

--- dragon-news-client/src/layout/Main.js
+++ dragon-news-client/src/layout/Main.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Col, Container, Row } from "react-bootstrap";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import LeftSideNav from "../pages/Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../pages/Shared/RightSideNav/RightSideNav";
+import Header from "../pages/Shared/Header/Header";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg="2">
+            <LeftSideNav></LeftSideNav>
+          </Col>
+          <Col lg="7">
             <Outlet></Outlet>
-        </div>
-    );
+          </Col>
+          <Col lg="3">
+            <RightSideNav></RightSideNav>
+          </Col>
+        </Row>
+      </Container>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
